a2a85124039f490df9c7de3a76ac27654a78eb51
Smith <theakperheovo@gmail.com>
2021-06-21 18:02:12 +0100

    Initial Commit

---
+++ app/src/main/java/com/ogheneovo/firstproject/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ogheneovo.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    val username : String ="admin"
+    val password : String ="admin"
+
+    private  lateinit var usernameInput : EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
